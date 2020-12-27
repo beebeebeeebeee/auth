008a5e7573d97346126deb48aec1cf0ae30258ce
beebeebeeebeee <bee.1845698825@gmail.com>
2020-12-28 03:43:26 +0800

    bug

--- router/auth.js
+++ router/auth.js
@@ -65,8 +65,7 @@ router.post("/data", async (req, res) => {
   const token = req.cookies["auth-token"];
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    console.log(getData(verified.id));
-    res.status(200).send(getData(verified.id));
+    res.status(200).send(await getData(verified.id));
   } catch (err) {
     console.log(err);
     res.status(400).send();
